a3262dadcf7f99670f887ee7ee8676d3ef23059e
Ruby Chen <ruby@yun-ru.com>
2017-04-29 18:08:30 +0800

    產品頁下方carousel

--- src/js/product-item.js
+++ src/js/product-item.js
@@ -1,7 +1,8 @@
-var $carousel = $(".carousel")
+var $productCarousel = $("#product-carousel")
+var $hotSaleCarousel = $("#hot-sale-carousel")
 var $imgItem = $(".thumb-imgs .img-item")
 
-$carousel.slick({
+$productCarousel.slick({
     dots: true,
     arrows: false,
     speed: 300,
@@ -12,7 +13,7 @@ $carousel.slick({
     nextArrow: `<button class="ctrl-arrow arrow-right"><i class="icon icon-right-arrow"></i></button>`,
 });
 
-$carousel.on('afterChange', (e, slick, index) => {
+$productCarousel.on('afterChange', (e, slick, index) => {
   $('.thumb-imgs').find('.img-item').removeClass('active')
   $($('.thumb-imgs').find('.img-item')[index]).addClass('active')
 });
@@ -20,5 +21,18 @@ $carousel.on('afterChange', (e, slick, index) => {
 $imgItem.on("click", (e) => {
     var $targetItem = $(e.target).closest('div')
     var $index = $imgItem.index($targetItem)
-    $carousel.slick("slickGoTo", $index)
+    $productCarousel.slick("slickGoTo", $index)
 })
+
+$hotSaleCarousel.slick({
+    dots: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    centerMode: false,
+    variableWidth: false,
+    prevArrow: `<button class="ctrl-arrow arrow-left"><i class="icon icon-left-arrow"></i></button>`,
+    nextArrow: `<button class="ctrl-arrow arrow-right"><i class="icon icon-right-arrow"></i></button>`,
+})
+
